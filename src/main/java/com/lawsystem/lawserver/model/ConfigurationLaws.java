3627328056e9b170113ed01c2a2c29454311b997
danbi <danbinnun1@gmail.com>
2021-05-04 13:42:17 +0300

    fixed bug in configuration laws

--- src/main/java/com/lawsystem/lawserver/model/ConfigurationLaws.java
+++ src/main/java/com/lawsystem/lawserver/model/ConfigurationLaws.java
@@ -2,12 +2,14 @@ package com.lawsystem.lawserver.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
 @AllArgsConstructor
 public class ConfigurationLaws {
+    @DBRef
     private Member president;
     private int TimeForLawsToPass;
 }
