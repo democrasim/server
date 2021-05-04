015a6aff2278f446ec01cb0505ac7616597ef765
AmiterCodes <amiter123king@gmail.com>
2021-05-04 07:52:21 +0300

    fix(ConfigurationLawsRepo): fixed singleton fetch, feat(Undertow): switched to undertow

--- src/main/java/com/lawsystem/lawserver/repo/ConfigurationLawsRepository.java
+++ src/main/java/com/lawsystem/lawserver/repo/ConfigurationLawsRepository.java
@@ -2,7 +2,10 @@ package com.lawsystem.lawserver.repo;
 
 import com.lawsystem.lawserver.model.ConfigurationLaws;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface ConfigurationLawsRepository extends MongoRepository<ConfigurationLaws, String> {
-    ConfigurationLaws get();
+
+    @Query("{}")
+    ConfigurationLaws findOne();
 }
